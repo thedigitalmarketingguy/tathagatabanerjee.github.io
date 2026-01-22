5a239370c94aa3e2c6a03d43797c6017a26b5af7
thedigitalmarketingguy <tathagatabanerjee.marketing@gmail.com>
2026-01-22 14:32:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const sections = document.querySelectorAll(".fade");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (scrollY >= sectionTop) {
+      section.classList.add("show");
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
